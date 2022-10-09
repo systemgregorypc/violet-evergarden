a433655fa54b119be255a26753f5cebc0b13290a
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-10-08 22:51:36 -0400

    Create languages.py

---
+++ neuronal/.idea/languages.py
@@ -0,0 +1,12 @@
+langs={
+	"01":"binario",
+  "es-VE":"Spanish - Venezuela",
+  "chi":"creahachipne",
+  "ru":"Russian",
+	"uk":"Ukrainian",
+  "ja":"Japanese",
+  "zh":"Chinese ",
+  "ar":"Arabic",
+  "en":"English",
+  "ca":"Catalan",
+    
