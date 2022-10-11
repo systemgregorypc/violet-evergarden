670437163c7b3167755100d8d37c0335bb2b8714
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-10-11 13:14:21 -0400

    Create ProcessSignedList.aspx.cs

---
+++ violet-evergarden -voice/violet-evergardeniaportal/Area/Sign/ProcessSignedList.aspx.cs
@@ -0,0 +1,65 @@
+using DBTools;
+using violetevergardenPortal.FactoryMethod;
+using violetevergardenPortal.Interface;
+using violetevergardenPortal.Repository;
+using violetevergardenPortal.Tools;
+using violetevergardenPortal.ViewModel;
+using violetevergardenPortal.ViewModel.Sign;
+using violetevergardenPortal.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace  violetevergardenPortal.Area.Sign
+{
+    public partial class ProcessSignedList : SignDataList
+    {
+        private RootRepository _rootRepo = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Authentication.HasResource(User.Identity.Name, "ProcessSignedList"))
+            {
+                Response.Redirect(@"/account/logon.aspx?ReturnUrl=%2f");
+            }
+            if (!IsPostBack)
+            {
+                PageTitle.Value = "主管簽核歷史紀錄 > 列表";
+                var paggerParms = WebUtils.ParseQueryString<PaggerParms>(Page.Request);
+                var signListParms = WebUtils.ParseQueryString<SignListParms>(Page.Request);
+                signListParms.Member = Authentication.GetMemberViewModel(User.Identity.Name);
+                signListParms.GridView = ProcessSignedGridView;
+                signListParms.TotalRowsCount = totalRowsCount;
+                signListParms.PaginationBar = paginationBar;
+                signListParms.NoDataTip = noDataTip;
+
+                //建構頁面
+                ConstructPage(signListParms, paggerParms, RepositoryFactory.CreateSignQueryRepo());
+
+                pageSizeSelect.Text = paggerParms.PageSize.ToString();
+                queryTextBox.Text = signListParms.QueryText;
+            }
+        }
+
+        public string GetFormUrl(object docID, object employeeID, object formID)
+        {
+            _rootRepo = RepositoryFactory.CreateRootRepo();
+            if (String.IsNullOrWhiteSpace((string)docID))
+            {
+                return "無此簽核號碼".ToAlertFormat();
+            }
+            else
+            {
+                //簽核紀錄查詢 > 列表的表單明細Url
+                var pageSet = ConfigUtils.ParsePageSetting("WorkflowListSetting");
+                var result = String.Format(pageSet[formID.ToString()], docID, employeeID);
+                return result;
+            }
+        }
+
+    }
+}
