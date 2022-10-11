0c40b01a2cd8bf9e82b3dcf64ab46067f5bc9cba
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-10-11 13:40:13 -0400

    Create ProcessWorkflowList.aspx.cs

---
+++ violet-evergarden -voice/violet-evergardeniaportal/Area/Sign/ProcessWorkflowList.aspx.cs
@@ -0,0 +1,50 @@
+using DBTools;
+using violetevergardenPortal.FactoryMethod;
+using violetevergardenPortal.Interface;
+using violetevergardenPortall.Repository;
+using violetevergardenPortal.Tools;
+using violetevergardenPortal.ViewModel;
+using violetevergardenPortal.ViewModel.Sign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace violetevergardenPortal.Area.Sign
+{
+	public partial class ProcessWorkflowList : SignDataList
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!Authentication.HasResource(User.Identity.Name, "ProcessWorkflowList"))
+			{
+				Response.Redirect(@"/account/logon.aspx?ReturnUrl=%2f");
+			}
+
+			if (!IsPostBack)
+			{
+				PageTitle.Value = "待簽核作業 > 列表";
+
+				// 取得 QueryString 
+				var paggerParms = WebUtils.ParseQueryString<PaggerParms>(Page.Request);
+				var signListParms = WebUtils.ParseQueryString<SignListParms>(Page.Request);
+				signListParms.Member = Authentication.GetMemberViewModel(User.Identity.Name);
+				signListParms.GridView = ProcessWorkflowGridView;
+				signListParms.TotalRowsCount = totalRowsCount;
+				signListParms.PaginationBar = paginationBar;
+				signListParms.NoDataTip = noDataTip;
+
+				//建構頁面
+				ConstructPage(signListParms, paggerParms, RepositoryFactory.CreateProcessWorkflowRepo());
+
+				pageSizeSelect.Text = paggerParms.PageSize.ToString();
+				queryTextBox.Text = signListParms.QueryText;
+			}
+
+		}
+	}
+}
