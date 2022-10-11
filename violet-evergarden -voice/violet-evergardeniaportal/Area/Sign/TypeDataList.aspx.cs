dab4c9227c4435c02073afc18c44390b707f040d
jose gregorio hernandez calderon <systemgregoryp.c@gmail.com>
2022-10-11 14:04:58 -0400

    Create TypeDataList.aspx.cs

---
+++ violet-evergarden -voice/violet-evergardeniaportal/Area/Sign/TypeDataList.aspx.cs
@@ -0,0 +1,48 @@
+using DBTools;
+using violetevergardenPortal.FactoryMethod;
+using violetevergardenPortal.Interface;
+using violetevergardenPortal.Repository;
+using violetevergardenPortal.Tools;
+using violetevergardenPortal.ViewModel.Sign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace violetevergardenPortal.Area.Sign
+{
+    public partial class TypeDataList : SignDataList
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Authentication.HasResource(User.Identity.Name, "TypeDataList"))
+            {
+                Response.Redirect(@"/account/logon.aspx?ReturnUrl=%2f");
+            }
+
+            if (!IsPostBack)
+            {
+                PageTitle.Value = "表單類型資料 > 列表";
+
+                // 取得 QueryString 
+                var paggerParms = WebUtils.ParseQueryString<PaggerParms>(Page.Request);
+                var signListParms = WebUtils.ParseQueryString<SignListParms>(Page.Request);
+                signListParms.GridView = TypeGridView;
+                signListParms.TotalRowsCount = totalRowsCount;
+                signListParms.PaginationBar = paginationBar;
+                signListParms.NoDataTip = noDataTip;
+
+                //建構頁面
+                ConstructPage(signListParms, paggerParms, RepositoryFactory.CreateTypeRepo());
+
+                pageSizeSelect.Text = paggerParms.PageSize.ToString();
+                queryTextBox.Text = signListParms.QueryText;
+            }
+
+        }
+    }
+}
